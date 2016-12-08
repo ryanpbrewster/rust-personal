491e3f8f7bf87c5f329efea6152bfabfba10aab1
Ryan Brewster <ryanpbrewster@gmail.com>
2016-12-08 00:32:17 -0800

    Introduce examples that solve each problem

---
+++ examples/example_001.rs
@@ -0,0 +1,7 @@
+extern crate project_euler;
+
+use project_euler::problems::problem_001;
+
+fn main() {
+	println!("{}", problem_001::solve(1000));
+}
---
+++ examples/example_002.rs
@@ -0,0 +1,7 @@
+extern crate project_euler;
+
+use project_euler::problems::problem_002;
+
+fn main() {
+	println!("{}", problem_002::solve(4_000_000));
+}
