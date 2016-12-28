8cf55ff8562e74d46d28561dab6ac12ab7e30b5c
Ryan Brewster <ryanpbrewster@gmail.com>
2016-12-27 18:29:33 -0800

    Solve problem 048, badly

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -23,3 +23,4 @@ pub mod problem_031;
 pub mod problem_032;
 pub mod problem_034;
 pub mod problem_040;
+pub mod problem_048;
---
+++ src/problems/problem_048.rs
@@ -0,0 +1,13 @@
+const MOD: u64 = 10_000_000_000;
+pub fn solve(n: u64) -> u64 {
+    // Sum[i^i, {i, 1, n}] % 10^10
+    (1..n + 1).map(|i| pow_mod(i, i, MOD)).sum::<u64>() % MOD
+}
+
+fn pow_mod(b: u64, e: u64, m: u64) -> u64 {
+    let mut n = 1;
+    for _ in 0..e {
+        n = (n * b) % m;
+    }
+    n
+}
