b3c45c7ca45b4d41fcce5828a0b86ed0de455f5f
Ryan Brewster <ryanpbrewster@gmail.com>
2016-12-27 21:32:31 -0800

    Explore solutions in descending order

--- src/problems/problem_050.rs
+++ src/problems/problem_050.rs
@@ -8,21 +8,29 @@ pub fn solve(hi: usize) -> usize {
     let sieve = primes::sieve(hi);
 
     let ps: Vec<usize> = (2..hi).filter(|&i| sieve[i]).collect();
+    let cum_sum: Vec<usize> = ps.iter()
+        .scan(0, |acc, v| {
+            *acc = *acc + v;
+            Some(*acc - v)
+        })
+        .collect();
 
-    let mut best_total = 2;
-    let mut best_range = 0;
-    for i in 0..ps.len() {
-        let mut j = i;
-        let mut total = 0;
-        while j < ps.len() && total < hi {
-            if sieve[total] && j - i > best_range {
-                best_total = total;
-                best_range = j - i;
+    let n_max = cum_sum.iter()
+        .enumerate()
+        .find(|&(_, v)| *v >= hi)
+        .map(|(idx, _)| idx)
+        .unwrap_or(ps.len());
+
+    for n in (1..n_max).rev() {
+        let end = (0..ps.len() - n)
+            .find(|&i| cum_sum[i + n] - cum_sum[i] >= hi)
+            .unwrap_or(ps.len() - n);
+        for start in 0..end {
+            let total = cum_sum[start + n] - cum_sum[start];
+            if total < hi && sieve[total] {
+                return total;
             }
-            total += ps[j];
-            j += 1;
         }
     }
-
-    best_total
+    return 0;
 }
