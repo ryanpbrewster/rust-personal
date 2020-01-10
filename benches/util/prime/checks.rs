6b29340e06e8eb5af60ee1d9f78dfac6ea430724
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-01-09 23:37:38 -0800

    Fix benchmarks as well

--- benches/util/prime/checks.rs
+++ benches/util/prime/checks.rs
@@ -1,13 +1,13 @@
 use project_euler::util::prime;
 use test::Bencher;
 
-use rand::{Rng, SeedableRng, XorShiftRng};
+use rand::{Rng, SeedableRng};
 
 #[bench]
 fn trial_division(bench: &mut Bencher) {
     const MIN: u64 = 1;
     const MAX: u64 = 1_000_000;
-    let mut prng = XorShiftRng::from_seed([42, 42, 42, 42]);
+    let mut prng = rand_pcg::Pcg32::seed_from_u64(42);
     bench.iter(|| prime::checks::trial_division(prng.gen_range(MIN, MAX)))
 }
 
@@ -17,6 +17,6 @@ fn sieve(bench: &mut Bencher) {
     const MAX: u64 = 1_000_000;
     let sieve = prime::sieve(MAX as usize);
 
-    let mut prng = XorShiftRng::from_seed([42, 42, 42, 42]);
+    let mut prng = rand_pcg::Pcg32::seed_from_u64(42);
     bench.iter(|| sieve[prng.gen_range(MIN, MAX) as usize])
 }
